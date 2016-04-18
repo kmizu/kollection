287ee0387fa9b6d279f9b69a1ffbf15ca160e5e1
Kota Mizushima <mizukota@gmail.com>
2016-04-19 01:34:52 +0900

    Refactor using block() method.

--- src/main/kotlin/com/github/kmizu/kollection/KList.kt
+++ src/main/kotlin/com/github/kmizu/kollection/KList.kt
@@ -1,4 +1,5 @@
 package com.github.kmizu.kollection
+import com.github.kmizu.kollection.kontrol.block
 
 sealed class KList<out T:Any>() {
     class KCons<out T:Any>(val head: T, val tail: KList<T>) : KList<T>() {
@@ -16,26 +17,26 @@ sealed class KList<out T:Any>() {
         }
         override fun toString(): String = "KNil"
     }
-    fun reverse(): KList<T> {
+    fun reverse(): KList<T> = block {
         tailrec fun loop(accumlator: KList<T>, rest: KList<T>): KList<T> = when(rest) {
             is KNil -> accumlator
             is KCons<T> -> loop(rest.head.prepend(accumlator), rest.tail)
         }
-        return loop(KNil, this)
+        loop(KNil, this)
     }
-    fun <U:Any> foldLeft(z: U, function: (U, T) -> U): U {
+    fun <U:Any> foldLeft(z: U, function: (U, T) -> U): U  = block {
         tailrec fun loop(list: KList<T>, accumulator: U): U = when(list) {
             is KCons<T> -> loop(list.tail, function(accumulator, list.head))
             is KNil -> accumulator
         }
-        return loop(this, z)
+        loop(this, z)
     }
-    fun <U:Any> map(function: (T) -> U): KList<U> {
+    fun <U:Any> map(function: (T) -> U): KList<U>  = block {
         tailrec fun loop(list: KList<T>, result: KList<U>): KList<U> = when(list) {
             is KCons<T> -> loop(list.tail, function(list.head) prepend result)
             is KNil -> result
         }
-        return loop(this, KNil).reverse()
+        loop(this, KNil).reverse()
     }
 }
 
