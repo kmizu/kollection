6241ac25e74936e2b0e3a26fa7ec208d964b5233
Kota Mizushima <mizukota@gmail.com>
2016-04-19 01:12:09 +0900

    Rename klistOf() to klist()

--- src/main/kotlin/com/github/kmizu/kollection/KListExtensions.kt
+++ src/main/kotlin/com/github/kmizu/kollection/KListExtensions.kt
@@ -3,7 +3,7 @@ import com.github.kmizu.kollection.KList.*
 
 infix fun <T:Any> T.prepend(other: KList<T>): KList<T> = KList.KCons(this, other)
 
-fun <T:Any> klistOf(vararg elements: T): KList<T> {
+fun <T:Any> klist(vararg elements: T): KList<T> {
     var result: KList<T> = KNil
     for(e in elements.reversed()) {
         result = e.prepend(result)
--- src/test/kotlin/com/github/kmizu/kollection/KlistSpec.kt
+++ src/test/kotlin/com/github/kmizu/kollection/KlistSpec.kt
@@ -9,18 +9,18 @@ class KListSpec(): Spek() {
     init {
         given("A KList") {
             on("which is a KCons") {
-                val klist = klistOf(1, 2, 3, 4, 5)
+                val klist = klist(1, 2, 3, 4, 5)
                 it("prepend()") {
                     assertEquals(klist, 1 prepend (2 prepend (3 prepend (4 prepend (5 prepend KNil)))))
                 }
                 it("reverse()") {
-                    assertEquals(klistOf(5, 4, 3, 2, 1), klist.reverse())
+                    assertEquals(klist(5, 4, 3, 2, 1), klist.reverse())
                 }
                 it("foldLeft()") {
                     assertEquals(15, klist.foldLeft(0){l, r -> l + r})
                 }
                 it("map()") {
-                    assertEquals(klistOf(2, 3, 4, 5, 6), klist.map {it + 1})
+                    assertEquals(klist(2, 3, 4, 5, 6), klist.map {it + 1})
                 }
             }
         }
