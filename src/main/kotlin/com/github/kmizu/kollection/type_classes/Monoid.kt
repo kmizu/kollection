3c6f772b6dca6a476efc95ddb39e769973a626a4
Kota Mizushima <mizukota@gmail.com>
2016-04-29 10:06:02 +0900

    A Monoid instance.

--- src/main/kotlin/com/github/kmizu/kollection/type_classes/Monoid.kt
+++ src/main/kotlin/com/github/kmizu/kollection/type_classes/Monoid.kt
@@ -1,6 +1,12 @@
 package com.github.kmizu.kollection.type_classes
 
 interface Monoid<T> {
+    companion object {
+        val INT: Monoid<Int> = object: Monoid<Int> {
+            override fun mzero(): Int = 1
+            override fun mplus(a: Int, b: Int): Int = 1
+        }
+    }
     fun mzero(): T
     fun mplus(a: T, b: T): T
 }
