93fdf9519b5e39ba9b22e724fe8d2c3ba54ee86c
Kota Mizushima <mizukota@gmail.com>
2016-04-30 17:48:30 +0900

    Improved tests descriptions

--- src/test/kotlin/com/github/kmizu/kollection/KStreamSpec.kt
+++ src/test/kotlin/com/github/kmizu/kollection/KStreamSpec.kt
@@ -6,57 +6,84 @@ import kotlin.test.assertFailsWith
 
 class KStreamSpec(): Spek() {
     init {
-        given("KStream") {
-            on("consisting of 1, 2, 3, and 4") {
-                it("is equal to 1 cons { 2 cons { 3 cons { 4 cons {KStreamNil}}}}") {
+        given("Args which consists of 1, 2, 3, and 4") {
+            on("performing KStream function") {
+                it("produces (1 cons { 2 cons { 3 cons { 4 cons {KStreamNil}}}})") {
                     assertEquals(1 cons { 2 cons { 3 cons { 4 cons {KStreamNil}}}}, KStream(1, 2, 3, 4))
                 }
             }
-            on("describing counter") {
-                var i = 0
-                fun countUp(): Int = run{ i += 1; i }
-                val counter: KStream<Int> = KStream.forever { countUp() }
-                it("take(3) produce KStream(1, 2, 3)") {
-                    assertEquals(KStream(1, 2, 3), counter.take(3))
-                }
-            }
-            on("KStream of 1, 2, 3, ... and KStream of 4, 5, 6, ...") {
-                val a = KStream.from(1)
-                val b = KStream.from(4)
-                it("flatMap()") {
-                    assertEquals(KStream(1, 4, 1, 5, 1, 6), a.flatMap{x -> b.flatMap {y -> KStream(x, y) }}.take(6))
-                }
-            }
-            on("describing fibonacci can be defined") {
-                fun fib(): KStream<Int> = 0 cons { 1 cons { fib() zip fib().tl map {it.first + it.second} } }
-                it("take(5) is KStream(0, 1, 1, 2, 3, 5, 8)") {
+        }
+        given("A KStream describing fibonacci number") {
+            fun fib(): KStream<Int> = 0 cons { 1 cons { fib() zip fib().tl map {it.first + it.second} } }
+            on("performing take(5)") {
+                it("produces KStream(0, 1, 1, 2, 3, 5, 8)") {
                     assertEquals(KStream(0, 1, 1, 2, 3, 5, 8), fib().take(7))
                 }
             }
-            on("describing an infinite consisting of natural numbers") {
-                val nat = KStream.from(0)
-                it("map{x -> x + 1}.take(3) produces KStream(1, 2, 3)") {
-                    assertEquals(KStream(1, 2, 3), nat.map { it + 1 }.take(3))
+        }
+        given("A KStream describing counter") {
+            var i = 0
+            fun countUp(): Int = run { i += 1; i }
+            val counter: KStream<Int> = KStream.forever { countUp() }
+            on("performing take") {
+                val result = counter.take(3)
+                it("produces KStream(1, 2, 3)") {
+                    assertEquals(KStream(1, 2, 3), result)
                 }
-                it("map{x -> x + 1}.drop(3) produces KStream(4, 5, 6, ...)") {
-                    assertEquals(KStream(4, 5, 6), nat.map { it + 1 }.drop(3).take(3))
+            }
+        }
+        given("A KStream describing an infinite stream of natural numbers") {
+            val nat = KStream.from(0)
+            on("performing take and toKList") {
+                val result = nat.take(3).toKList()
+                it("produces KList(0, 1, 2)") {
+                    assertEquals(KList(0, 1, 2), result)
                 }
-                it("takeWhile{x -> x < 6} produces KStream(0, 1, 2, 3, 4, 5)") {
-                    assertEquals(KStream(0, 1, 2, 3, 4, 5), nat.takeWhile{it <  6})
+            }
+            on("performing map") {
+                val result = nat.map{x -> x + 1}.take(3)
+                it("produces KStream(1, 2, 3, ...)") {
+                    assertEquals(KStream(1, 2, 3), result)
                 }
-                it("dropWhile{x -> x < 2} produces KStream(2, 3, 4, 5, ...)") {
-                    assertEquals(KStream(2, 3, 4, 5), nat.dropWhile {it < 2}.take(4))
+            }
+            on("performing map and drop") {
+                val result = nat.map{x -> x + 1}.drop(3).take(3)
+                it("produces KStream(4, 5, 6, ...)") {
+                    assertEquals(KStream(4, 5, 6), result)
                 }
-                it("calculate sum of first 5 numbers using foldLeft()") {
+            }
+            on("performing takeWhile") {
+                val result = nat.takeWhile {it < 6}
+                it("produces KStream(0, 1, 2, 3, 4, 5)") {
+                    assertEquals(KStream(0, 1, 2, 3, 4, 5), result)
+                }
+            }
+            on("performing dropWhile") {
+                val result = nat.dropWhile {it < 2}.take(4)
+                it("produces KStream(2, 3, 4, 5, ...)") {
+                    assertEquals(KStream(2, 3, 4, 5), result)
+                }
+            }
+            on("calculating sum of first 5 elements using foldLeft") {
+                it("returns 10") {
                     assertEquals(10, nat.take(5).foldLeft(0){a, b -> a + b})
                 }
-                it("toKList() should throw StackOverflowError") {
+            }
+            on("performing toKList") {
+                it("throws StackOverFlowError") {
                     assertFailsWith(StackOverflowError::class) {
                         nat.toKList()
                     }
                 }
-                it("take(3).toKList() is KList(0, 1, 2)") {
-                    assertEquals(KList(0, 1, 2), nat.take(3).toKList())
+            }
+        }
+        given("Two KStream") {
+            val a = KStream.from(1)
+            val b = KStream.from(4)
+            on("performing flatMap") {
+                val result = a.flatMap{x -> b.flatMap {y -> KStream(x, y) }}.take(6)
+                it("produces KStream(1, 4, 1, 5, 1, 6, ...)") {
+                    assertEquals(KStream(1, 4, 1, 5, 1, 6), result)
                 }
             }
         }
