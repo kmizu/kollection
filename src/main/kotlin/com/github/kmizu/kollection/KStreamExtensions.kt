a3ea33372df95f441957a52b8d92867459867a82
Kota Mizushima <mizukota@gmail.com>
2016-04-30 13:51:08 +0900

    <T> KStream<T>.concat(KStream<T>) extension method.

--- src/main/kotlin/com/github/kmizu/kollection/KStreamExtensions.kt
+++ src/main/kotlin/com/github/kmizu/kollection/KStreamExtensions.kt
@@ -2,3 +2,6 @@ package com.github.kmizu.kollection
 import com.github.kmizu.kollection.KStream.*
 
 infix fun <T> T.cons(other: () -> KStream<T>): KStream<T> = KStreamCons(this, other)
+infix fun <T> KStream<T>.concat(other: KStream<T>): KStream<T> = run {
+    if(isEmpty) other else this.hd cons { this.tl concat other }
+}
