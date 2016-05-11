751a869ad944d15be0f33bbd47af5f1bd54978a2
Kota Mizushima <mizukota@gmail.com>
2016-05-11 23:53:04 +0900

    Shorter names.

--- src/main/kotlin/com/github/kmizu/kollection/KTreeSet.kt
+++ src/main/kotlin/com/github/kmizu/kollection/KTreeSet.kt
@@ -1,7 +1,7 @@
 package com.github.kmizu.kollection
 import com.github.kmizu.kollection.Tree.*
 
-class KTreeSet<T>(val comparator: (T, T) -> Int, val value: Tree<T> = Tree.Empty) : KSet<T> {
+class KTreeSet<T>(val comparator: (T, T) -> Int, val value: Tree<T> = Tree.E) : KSet<T> {
     companion object {
         val BYTE_COMPARATOR: (Byte, Byte) -> Int = {a, b -> if(a < b) -1 else if(a > b) 1 else 0}
         val SHORT_COMPARATOR: (Short, Short) -> Int = {a, b -> if(a < b) -1 else if(a > b) 1 else 0}
@@ -13,8 +13,8 @@ class KTreeSet<T>(val comparator: (T, T) -> Int, val value: Tree<T> = Tree.Empty
     }
     override fun get(element: T): Boolean = run {
         fun contains(e: T, t: Tree<T>): Boolean = when(t) {
-            is Tree.Empty -> false
-            is Tree.Node<T> -> {
+            is Tree.E -> false
+            is Tree.N<T> -> {
                 if (comparator(e, t.e) < 0) contains(e, t.l)
                 else if(comparator(t.e, e) < 0) contains(e, t.r)
                 else true
@@ -23,40 +23,40 @@ class KTreeSet<T>(val comparator: (T, T) -> Int, val value: Tree<T> = Tree.Empty
         contains(element, this.value)
     }
 
-    private fun balance(c: Color, l: Tree<T>, e: T, r: Tree<T>): Node<T> = run {
-        if(c == Color.BLACK && l is Node<T> && l.c == Color.RED && l.l is Node<T> && l.l.c == Color.RED)
-            Node(Color.RED, Node(Color.BLACK, l.l.l, l.l.e, l.l.r), l.e, Node(Color.BLACK, l.r, e, r))
-        else if(c == Color.BLACK && l is Node<T> && l.c == Color.RED && l.r is Node<T> && l.r.c == Color.RED)
-            Node(Color.RED, Node(Color.BLACK, l.l, l.e, l.r.l), l.r.e, Node(Color.BLACK, l.r.l, e, r))
-        else if(c == Color.BLACK && r is Node<T> && r.c == Color.RED && r.l is Node<T> && r.l.c == Color.RED)
-            Node(Color.RED, Node(Color.BLACK, l, e, r.l.l), r.l.e, Node(Color.BLACK, r.l.r, r.e, r.r))
-        else if(c == Color.BLACK && r is Node<T> && r.c == Color.RED && r.r is Node<T> && r.r.c == Color.RED)
-            Node(Color.RED, Node(Color.BLACK, l, e, r.l), r.e, Node(Color.BLACK, r.r.l, r.r.e, r.r.r))
+    private fun balance(c: Color, l: Tree<T>, e: T, r: Tree<T>): N<T> = run {
+        if(c == Color.BLACK && l is N<T> && l.c == Color.RED && l.l is N<T> && l.l.c == Color.RED)
+            N(Color.RED, N(Color.BLACK, l.l.l, l.l.e, l.l.r), l.e, N(Color.BLACK, l.r, e, r))
+        else if(c == Color.BLACK && l is N<T> && l.c == Color.RED && l.r is N<T> && l.r.c == Color.RED)
+            N(Color.RED, N(Color.BLACK, l.l, l.e, l.r.l), l.r.e, N(Color.BLACK, l.r.l, e, r))
+        else if(c == Color.BLACK && r is N<T> && r.c == Color.RED && r.l is N<T> && r.l.c == Color.RED)
+            N(Color.RED, N(Color.BLACK, l, e, r.l.l), r.l.e, N(Color.BLACK, r.l.r, r.e, r.r))
+        else if(c == Color.BLACK && r is N<T> && r.c == Color.RED && r.r is N<T> && r.r.c == Color.RED)
+            N(Color.RED, N(Color.BLACK, l, e, r.l), r.e, N(Color.BLACK, r.r.l, r.r.e, r.r.r))
         else
-            Node(c, l, e, r)
+            N(c, l, e, r)
     }
 
     override operator fun plus(element: T): KTreeSet<T> = run {
-        fun insert(tree: Tree<T>): Node<T> = when(tree) {
-            is Empty -> Node(Color.RED, Empty, element, Empty)
-            is Node -> {
+        fun insert(tree: Tree<T>): N<T> = when(tree) {
+            is E -> N(Color.RED, E, element, E)
+            is N -> {
                 if(comparator(element, tree.e) < 0) balance(tree.c, insert(tree.l), tree.e, tree.r)
                 else if(comparator(tree.e, element) < 0) balance(tree.c, tree.l, tree.e, insert(tree.r))
                 else tree
             }
         }
         val t = insert(value)
-        KTreeSet(comparator, Node(Color.BLACK, t.l, t.e, t.r))
+        KTreeSet(comparator, N(Color.BLACK, t.l, t.e, t.r))
     }
 
     override val isEmpty: Boolean
-        get() = value is Tree.Empty
+        get() = value is Tree.E
 
     override fun iterator(): Iterator<T> = object: Iterator<T> {
         private var list: KList<T> = listFrom(value)
         fun listFrom(tree: Tree<T>): KList<T> = when(tree) {
-            is Empty -> KList.Nil
-            is Node -> listFrom(tree.l) concat KList(tree.e) concat listFrom(tree.r)
+            is E -> KList.Nil
+            is N -> listFrom(tree.l) concat KList(tree.e) concat listFrom(tree.r)
         }
         override fun hasNext(): Boolean = list is KList.Cons
 
@@ -68,9 +68,10 @@ class KTreeSet<T>(val comparator: (T, T) -> Int, val value: Tree<T> = Tree.Empty
     }
 
     override fun remove(element: T): KTreeSet<T> = run {
-        fun findTarget(e: T, tree: Tree<T>): Node<T>? = when(tree) {
-            is Empty -> null
-            is Node<T> -> {
+
+        fun findTarget(e: T, tree: Tree<T>): N<T>? = when(tree) {
+            is E -> null
+            is N<T> -> {
                 if(comparator(e, tree.e) < 0) findTarget(e, tree.l)
                 else if(comparator(tree.e, e) < 0) findTarget(e, tree.r)
                 else tree
@@ -78,7 +79,7 @@ class KTreeSet<T>(val comparator: (T, T) -> Int, val value: Tree<T> = Tree.Empty
         }
         val target = findTarget(element, value)
         when(target) {
-            null -> KTreeSet(comparator, Empty)
+            null -> KTreeSet(comparator, E)
             else -> {
                 TODO()
             }
--- src/main/kotlin/com/github/kmizu/kollection/KTreeSetExtensions.kt
+++ src/main/kotlin/com/github/kmizu/kollection/KTreeSetExtensions.kt
@@ -2,15 +2,15 @@ package com.github.kmizu.kollection
 
 enum class Color {RED, BLACK}
 sealed class Tree<out T> {
-    object Empty: Tree<Nothing>()
-    class Node<T>(val c: Color, val l: Tree<T>, val e: T, val r: Tree<T>): Tree<T>()
+    object E : Tree<Nothing>()
+    class N<T>(val c: Color, val l: Tree<T>, val e: T, val r: Tree<T>): Tree<T>()
 }
 fun <T:Comparable<T>> KTreeSet(vararg elements: T): KTreeSet<T> = run {
     val comparator = {a: T, b: T -> a.compareTo(b)}
     KTreeSet(comparator, *elements)
 }
 fun <T> KTreeSet(comparator: (T, T) -> Int, vararg elements: T): KTreeSet<T> = run {
-    var set: KTreeSet<T> = KTreeSet(comparator, Tree.Empty)
+    var set: KTreeSet<T> = KTreeSet(comparator, Tree.E)
     for(e in elements) {
         set = set + e
     }
